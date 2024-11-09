37d301f39434bbeaf174f757c7d76f48dee7420b
Samoylov Ilya <sia.ilya.dev@gmail.com>
2024-11-09 15:47:38 +0300

    Init project (winapp via C#, webapp via Vue+TS)

---
+++ app/winapp/kizessleep/kizessleep/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace kizessleep
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.txtResponse = new System.Windows.Forms.TextBox();
+            this.lblServerUrl = new System.Windows.Forms.Label();
+            this.txtServerUrl = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(12, 41);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectFile.TabIndex = 0;
+            this.btnSelectFile.Text = "Выбрать файл";
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(93, 42);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.ReadOnly = true;
+            this.txtFilePath.Size = new System.Drawing.Size(379, 20);
+            this.txtFilePath.TabIndex = 1;
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(12, 70);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(75, 23);
+            this.btnUpload.TabIndex = 4;
+            this.btnUpload.Text = "Загрузить";
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // txtResponse
+            // 
+            this.txtResponse.Location = new System.Drawing.Point(12, 99);
+            this.txtResponse.Multiline = true;
+            this.txtResponse.Name = "txtResponse";
+            this.txtResponse.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResponse.Size = new System.Drawing.Size(460, 200);
+            this.txtResponse.TabIndex = 5;
+            // 
+            // lblServerUrl
+            // 
+            this.lblServerUrl.Location = new System.Drawing.Point(12, 15);
+            this.lblServerUrl.Name = "lblServerUrl";
+            this.lblServerUrl.Size = new System.Drawing.Size(75, 13);
+            this.lblServerUrl.TabIndex = 2;
+            this.lblServerUrl.Text = "URL сервера:";
+            // 
+            // txtServerUrl
+            // 
+            this.txtServerUrl.Location = new System.Drawing.Point(93, 12);
+            this.txtServerUrl.Name = "txtServerUrl";
+            this.txtServerUrl.Size = new System.Drawing.Size(379, 20);
+            this.txtServerUrl.TabIndex = 3;
+            this.txtServerUrl.Text = "http://localhost:8080/upload";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 379);
+            this.Controls.Add(this.btnSelectFile);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.lblServerUrl);
+            this.Controls.Add(this.txtServerUrl);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.txtResponse);
+            this.Name = "Form1";
+            this.Text = "Загрузчик EDF файлов";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.TextBox txtResponse;
+        private System.Windows.Forms.Label lblServerUrl;
+        private System.Windows.Forms.TextBox txtServerUrl;
+    }
+}
---
+++ app/winapp/kizessleep/kizessleep/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Windows.Forms;
+
+namespace kizessleep
+{
+    public partial class Form1 : Form
+    {
+        private string selectedFilePath;
+        private readonly HttpClient client;
+
+        public Form1()
+        {
+            InitializeComponent();
+            client = new HttpClient();
+        }
+
+        private async void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "EDF files (*.edf)|*.edf|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    selectedFilePath = openFileDialog.FileName;
+                    txtFilePath.Text = selectedFilePath;
+                }
+            }
+        }
+
+        private async void btnUpload_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(selectedFilePath))
+            {
+                MessageBox.Show("Пожалуйста, сначала выберите файл.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            btnUpload.Enabled = false;
+            txtResponse.Text = "Загрузка...";
+
+            try
+            {
+                using (var formData = new MultipartFormDataContent())
+                {
+                    var fileContent = new ByteArrayContent(File.ReadAllBytes(selectedFilePath));
+                    formData.Add(fileContent, "file", Path.GetFileName(selectedFilePath));
+
+                    var response = await client.PostAsync(txtServerUrl.Text, formData);
+                    Console.WriteLine(response);
+                    var responseText = await response.Content.ReadAsStringAsync();
+
+
+                    txtResponse.Text = responseText;
+                }
+            }
+            catch (Exception ex)
+            {
+                txtResponse.Text = $"Ошибка: {ex.Message}";
+            }
+            finally
+            {
+                btnUpload.Enabled = true;
+            }
+        }
+    }
+}
